7091314a8a7cee9aaa084259b157c7c551545bbd
Hoang <cct.cry@gmai.com>
2019-10-01 09:53:15 +0700

    up file

---
+++ dayinmonth.js
@@ -0,0 +1,28 @@
+
+function tinhsongay() {
+    let month = Number(document.getElementById("month").value);
+
+    switch (month) {
+
+        case 1:
+        case 3:
+        case 5:
+        case 7:
+        case 8:
+        case 10:
+        case 12:
+            document.getElementById("hienthi").innerHTML="Tháng "+ month + " có 31 ngày";
+            break;
+        case 2:
+            document.getElementById("hienthi").innerHTML="Tháng "+ + month +" có 28 hoặc 29 ngày";
+            break;
+        case 4:
+        case 6:
+        case 9:
+        case 11:
+            document.getElementById("hienthi").innerHTML="Tháng " + month +" có 30 ngày";
+            break;
+
+
+    }
+}
